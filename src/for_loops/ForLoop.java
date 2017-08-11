670778c21b5d0bd0772a2d6eb98c921e3c1d7455
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-11 14:27:50 -0700

    fourth day
    asdfasdfasdfasdfasdfasdfasdfadsfsdf

--- src/for_loops/ForLoop.java
+++ src/for_loops/ForLoop.java
@@ -1,10 +1,14 @@
 package for_loops;
 
 public class ForLoop {
-public static void main(String[] args) {
-	for(int i=100;i>=0;i--)
-	System.out.println(i);
-	for(int i=0;i>=100;i--)
-		System.out.println(i);
-}
+	public static void main(String[] args) {
+		for (int i = 100; i >= 0; i--)
+			System.out.println(i);
+		for (int i = 0; i >= 100; i--)
+			System.out.println(i);
+
+		for (int i = 0; i < .; i++) {
+			System.out.println();
+		}
+	}
 }
